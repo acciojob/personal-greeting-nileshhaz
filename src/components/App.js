70a36d34953c6d230f10e31d740a17dd040e978c
Nilesh Hazra <nileshhazra.nh@gmail.com>
2025-03-20 20:33:25 +0530

    change files 2

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <label htmlFor="name">Enter your name:</label>
       <input type="text" id="name" onChange={handleChange} value={name} />
-      <div></div>
+      <div>{name}</div>
     </div>
   )
 }
